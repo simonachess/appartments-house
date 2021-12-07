f91851fc21669ce02dd90a45c5c3cd3c7f9a624a
simonachess <simonachess@gmail.com>
2021-12-07 14:58:54 +0200

    react-query-intro

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,44 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useLocalStorage } from "../useLocalStorage";
 
-const Category = ({ index, title, subcategories, parentLength }) => {
 
-    const [checked, setChecked] = useState(false);
-    const [checkedChildren, setCheckedChildren] = useState([]);
+const Category = ({ index, title, subcategories }) => {
+
+    const [checkedChildren, setCheckedChildren] = useLocalStorage("checked", []);
 
     const handleChange = (e) => {
-        setChecked(e.target.checked)
-        if (e.target.checked) {
-            setCheckedChildren(subcategories.map(i => i.id))
+        const subCatIds = subcategories.map(i => i.id);
+        if (subCatIds.length === checkedChildren.length) {
+            setCheckedChildren([])
         } else {
-            setCheckedChildren([]);
+            setCheckedChildren(subCatIds)
         }
     }
 
     const handleChildChange = (e, id) => {
         let checkedChildrenCopy = e.target.checked ? [...checkedChildren, id] : checkedChildren.filter(item => item !== id)
-        const state = parentLength === checkedChildrenCopy.length;
         setCheckedChildren(checkedChildrenCopy);
-        setChecked(state);
+        localStorage.setItem("checked", JSON.stringify(checkedChildrenCopy));
     }
 
-    return (
-        <div key={index} >
-            <input type="checkbox" onChange={handleChange} checked={checked}></input>
-            <span>{title}
-            </span>
-            {subcategories.map(i => {
-                let checkedChildrenState = checkedChildren.indexOf(i.id) !== -1
-                return (
-                    <ul key={i.id}>
-                        <div>
-                            <span>{i.name}</span>
-                            <input type="checkbox" checked={checkedChildrenState} onChange={(e) => handleChildChange(e, i.id)}></input>
-                        </div>
-                    </ul>
-                )
-            })}
-        </div>
-    )
+    if (localStorage.length) {
+        return (
+            <div key={index} >
+                <input type="checkbox" onChange={handleChange} checked={checkedChildren.length === subcategories.length}></input>
+                <span>{title}
+                </span>
+                {subcategories.map(i => {
+                    let checkedChildrenState = checkedChildren.indexOf(i.id) !== -1
+                    return (
+                        <ul key={i.id}>
+                            <div>
+                                <span>{i.name}</span>
+                                <input type="checkbox" checked={checkedChildrenState} onChange={(e) => handleChildChange(e, i.id)}></input>
+                            </div>
+                        </ul>
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
 export default Category
